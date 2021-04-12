9e7d21239378b253191a99d4c05e3bdea2aaeaf8
Dorayya-chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-04-12 15:13:39 +0530

    Add files via upload

---
+++ 2d_array_max.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int arr[100][100],max,i,j,row_size,col_size;
+	scanf("%d%d",&row_size,&col_size);
+	for(i=0;i<row_size;i++)
+	{
+		for(j=0;j<col_size;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	max=arr[0][j];
+	for(j=0;i<row_size;i++)
+	{
+		
+		for(j=0;j<col_size;j++)
+		{
+			if(max<arr[i][j])
+			{
+				max=arr[i][j];
+				
+			}
+			//printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+        printf("%d",max);	
+	}
+	//printf("%d",max);
+	return 0;
+}
