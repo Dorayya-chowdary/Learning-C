df7a82f6e7b1d56e53b5fe3c8ed05f3ee9991e0b
Dorayya-chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-04-19 18:54:04 +0530

    Add files via upload

---
+++ String_lengths.c
@@ -0,0 +1,21 @@
+//string lengths
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100][100];
+	int i,size,len;
+	printf("enter size\n");
+	scanf("%d",&size);
+		for(i=0;i<size;i++)
+	{
+		printf("enter string %d\n",i+1);
+		scanf(" %[^\n]s",&str[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		len=strlen(str[i]);
+		printf("Length of the string %d =%d\n",i+1,len);
+	}
+   	   
+}
